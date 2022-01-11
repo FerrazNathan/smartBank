308fee2e1a53a3a8a3d7c6e9160a360c7cffeb02
Nathan Ferraz <nathanferraz@hotmail.com>
2022-01-11 16:26:51 -0300

    Refatoraçao do estilo

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,17 +1,32 @@
 import React from "react";
-
+import styled from "styled-components";
 import Titulo from "../Titulo";
 import Conta from "../Conta";
 
-const Container = () => {
+export const Container = styled.div`
+  background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`
+
+export const Content = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  @media (max-width: 800px) {
+    flex-direction: column
+  }
+`
+
+export default() => {
   return (
-    <div className="container">
-      <Titulo>Olá Fulano!</Titulo>
-      <section className="conteudo">
+    <Container>
+      <Titulo>Hello World!</Titulo>
+      <Content>
         <Conta />
-      </section>
-    </div>
+      </Content>
+    </Container>
   );
 };
 
-export default Container;
---
+++ src/Components/UI/variaveis.js
@@ -0,0 +1,3 @@
+export const colorPrimary = "#41d3be";
+export const lightBackground = "#f1f1f1";
+export const textLightBackgorund = "#cccccc"
