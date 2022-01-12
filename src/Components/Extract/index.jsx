70f18ee634b63ddc378368e8b765fe518ca214ac
Nathan Ferraz <nathanferraz@hotmail.com>
2022-01-12 09:48:02 -0300

    Criacao do Box e dos componetes de Item

--- src/Components/Extract/index.jsx
+++ src/Components/Extract/index.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { Box, Button } from "../UI";
 import { extratoLista } from '../../info';
+import Items from "../Items";
 
 const Extract = () => {
   return (
     <Box>
-      {
-        extratoLista.updates.map(({id, type, from, value, date}) => {
-          return(
-            <div key={id}>
-              <div>{type}</div>
-              <div>{from}</div>
-            </div>
-          )
-        })
+      {extratoLista.updates.map(({ id, type, from, value, date }) => {
+        return (
+          <Items
+            key={id}
+            type={type}
+            from={from}
+            value={value}
+            date={date}
+          />
+        )
+      })
       }
       <Button>Ver Mais</Button>
     </Box>
